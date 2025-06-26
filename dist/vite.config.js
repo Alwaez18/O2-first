22adf9dfb48f7d996d42b338a7f8e25cd7341c01
Alwaez18 <tuktukin@bk.ru>
2025-06-26 10:29:40 +0300

    nuxt

---
+++ dist/vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    build: {
+      outDir: 'dist',  // куда складывать результат
+      target: 'es2015' // поддержка браузеров
+    }
+  }
---
+++ index.js
@@ -0,0 +1,2 @@
+const _ = require('lodash');
+console.log(_.reverse([1, 2, 3])); // [3, 2, 1]
